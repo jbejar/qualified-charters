947982480e0d5c7690a5770cb858999c0192bae0
Jonathan Bejarano <jonathan.bejarano@coxautoinc.com>
2020-02-09 21:41:55 -0700

    basic layout

--- src/App.js
+++ src/App.js
@@ -3,29 +3,45 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import SchoolPage from './components/SchoolPage';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import { LinkContainer } from 'react-router-bootstrap'
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/schools/110575">School</Link>
-            </li>
-          </ul>
-        </nav>
+      <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+          <LinkContainer to="/about"><Nav.Link>About</Nav.Link></LinkContainer>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <LinkContainer to="/schools/110575">
+            <Button variant="outline-success">Search</Button>
+          </LinkContainer>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
