d2d18725e51c90220ddaa014b49669c1a2f223eb
Jonathan Bejarano <jonathan.bejarano@coxautoinc.com>
2020-02-15 14:28:56 -0700

    adding basic React charts functionality

--- src/components/SchoolPage.js
+++ src/components/SchoolPage.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import schools from './../dump.json';
+import LicenseComponent from './LicenseComponent';
 
 function SchoolPage(props) {
+    const id = parseInt(props.match.params.schoolID);
+    const school = schools.find(s => s.SchoolID === id);
+    
+    if(!school) {
+        return <div>404 - Could not find school</div>
+    }
     return (
-        <div>
-            <h2>Users {props.match.params.schoolID}</h2>;
+        <div class="container">    
+            <div class="jumbotron">
+                <h1 class="display-3">{school.SchoolName}</h1>
+    <p class="lead">{school.SchoolCategory} ({school.GradeLow} - {school.GradeHigh})</p>
+                <hr class="my-2"/>
+                <p><LicenseComponent types={school.licenseTypes}/></p>
+                <p>{school.Address} <br/>{school.City}, UT {school.Zip}</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" target="_blank" href={school.URL} role="button">Website</a>
+                </p>
+            </div>
+            <div>
+            </div>
         </div>
     )
 }
