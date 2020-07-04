3caf15af7d781bb2c8fa26acdcf9cdfad5c6ff7b
Jonathan Bejarano <jonathan.bejarano@coxautoinc.com>
2020-07-04 09:39:24 -0600

    lea specific license

--- src/components/SchoolTable.js
+++ src/components/SchoolTable.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 function SchoolTable(props) {
     const percentLicensed = s => 1 - ((s.licenseTypes["No License"] || 0) / (s.licenseTypes.All || 0))
     const temporaryLicensed = s =>  ((s.licenseTypes.Temporary || 0) / (s.licenseTypes.All || 0))
+    const leaLicensed = s =>  (((s.licenseTypes["Level 1 LEA-Specific"] || 0) + (s.licenseTypes["Level 2 LEA-Specific"] || 0)) / (s.licenseTypes.All || 0))
     const level2Up = s =>  (((s.licenseTypes["2"] || 0) + (s.licenseTypes["3"] || 0)) / (s.licenseTypes.All || 0))
     schools.sort((s,t) => (percentLicensed(t) - temporaryLicensed(t) + level2Up(t)) -(percentLicensed(s) - temporaryLicensed(s) + level2Up(s)))
   return (
@@ -17,9 +18,9 @@ function SchoolTable(props) {
           <tr>
             <th>#</th>
             <th>School</th>
-            <th>Licensed %</th>
-            <th>Level 2+ %</th>
-            <th>Temporary %</th>
+            <th>Licensed</th>
+            <th>Level 2+</th>
+            <th>Temporary</th>
             
           </tr>
         </thead>
@@ -30,6 +31,31 @@ function SchoolTable(props) {
               <td><Link to={"schools/"+school.SchoolID}>{school.SchoolName}</Link></td>
               <td>{(percentLicensed(school) * 100 ).toFixed(2)}%</td>
               <td>{(level2Up(school) * 100 ).toFixed(2)}%</td>
+              <td>{(temporaryLicensed(school) * 100 ).toFixed(2)}%</td>
+              
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <h4>Most LEA Specific Licenses</h4>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>School</th>
+            <th>LEA Licensed </th>
+            <th>Licensed</th>
+            <th>Temporary</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          {schools.sort((s,t) => (leaLicensed(t) -leaLicensed(s))).slice(0, 20).map((school, i) => (
+            <tr>
+              <td>{i+1}</td>
+              <td><Link to={"schools/"+school.SchoolID}>{school.SchoolName}</Link></td>
+              <td>{(leaLicensed(school) * 100 ).toFixed(2)}%</td>
+              <td>{(percentLicensed(school) * 100 ).toFixed(2)}%</td>
               <td>{(temporaryLicensed(school) * 100 ).toFixed(2)}%</td>
               
             </tr>
