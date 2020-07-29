ae8372d32a76d353b79eb169c62832219c90d3e4
Jonathan Bejarano <jonathan.bejarano@coxautoinc.com>
2020-07-29 12:32:07 -0600

    Updates

--- precache-manifest.8f631f4ad188f2d803869d62f6c39c1a.js
+++ precache-manifest.8f631f4ad188f2d803869d62f6c39c1a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ac24c2728495a1c8aead9e7ea7f3f19c",
+    "revision": "0aad85be88730d615359cd1f594f30f3",
     "url": "/qualified-charters/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/qualified-charters/static/css/2.98733928.chunk.css"
   },
   {
-    "revision": "a646c46105945f89ab07",
+    "revision": "d63c739b8dd022d7f0b3",
     "url": "/qualified-charters/static/css/main.d1b05096.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/qualified-charters/static/js/2.5839a5da.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a646c46105945f89ab07",
-    "url": "/qualified-charters/static/js/main.9735d097.chunk.js"
+    "revision": "d63c739b8dd022d7f0b3",
+    "url": "/qualified-charters/static/js/main.8b475e45.chunk.js"
   },
   {
     "revision": "afa2bdc789c9b1ba2e68",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/qualified-charters/precache-manifest.f4730fe41df34c2ff4740819342dd5ef.js"
+  "/qualified-charters/precache-manifest.8f631f4ad188f2d803869d62f6c39c1a.js"
 );
 
 self.addEventListener('message', (event) => {
