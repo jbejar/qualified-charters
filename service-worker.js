2fc6990aa6adebd42de4cb0652d8e8cb948c5bc4
Jonathan Bejarano <jonathan.bejarano@coxautoinc.com>
2020-09-14 08:35:41 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c9aff79556257c3c9fb50912e73d9e9.js"
+  "/precache-manifest.6cff9bc480fdb4ccdf27fb30316f206b.js"
 );
 
 self.addEventListener('message', (event) => {
