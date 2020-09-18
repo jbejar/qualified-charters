7248a4d27440b08039f1c685141dfd070be02330
Jonathan Bejarano <jonathan.bejarano@coxautoinc.com>
2020-09-18 13:18:19 -0600

    update

---
+++ src/pages/BlogPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Row from "react-bootstrap/Row";
+
+export default function BlogPage() {
+  return (
+    <div className="container">
+      <div className="jumbotron p-4">
+        <h1 className="display-4">Qualified Charters</h1>
+        <p className="lead my-4">
+          Blog
+        </p>
+
+      </div>
+      <Row className="mb-2">
+
+      </Row>
+    </div>
+  );
+}
