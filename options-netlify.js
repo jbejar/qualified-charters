91c90ea6033d4677d93457d45a4d188d85f14548
Jonathan Bejarano <jonathan.bejarano@coxautoinc.com>
2020-09-20 07:36:01 -0600

    adding options

--- options-netlify.js
+++ options-netlify.js
@@ -1,3 +1,5 @@
 require("./src/dump.json").forEach(s => {
     console.log(`     - { label: "${s.SchoolName}", value: "${s.SchoolID}" }`)
 })
+console.log(`- { label: "Qualified", value: "qualified" }`)
+console.log(`- { label: "Transparency", value: "transparency" }`)
